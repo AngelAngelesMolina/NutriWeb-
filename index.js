d7ca2fd0172a3c5d6ccbc49feb4e94f9090bb23b
Angel Angeles <angel.angeles.molina@gmail.com>
2023-11-26 14:38:25 -0600

    cors

--- index.js
+++ index.js
@@ -18,11 +18,18 @@ const app = express();
 /**
  * Incluye los agentes intermedios.
  */
-const cors = require("cors");
+//const cors = require("cors");
 const validationError = require("./middlewares/validation-error");
 const unknownError = require("./middlewares/unknown-error");
 
-app.use(cors());
+//app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'https://melodious-gelato-c9056b.netlify.app'); // Reemplaza con la URL de tu frontend
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(express.json());
 app.use(validationError);
 app.use(unknownError);
